002abdc2438ae8641008d75e3a35614cd839e5a1
Daniel Huffman <drhuffman12@gmail.com>
2026-01-26 21:15:58 -0500

    restart

---
+++ spec/ai4cr3/neural_network/backpropagation_spec.cr
@@ -0,0 +1,11 @@
+require "../spec_helper"
+require "../../src/ai4cr3/neuonetwork/backpropogarion.cr"
+
+
+describe Ai4cr3::Neuronetwork::backpropogation do
+  # TODO: Write tests
+
+  it "works" do
+    true.should eq(true)
+  end
+end
---
+++ src/ai4cr3.cr
@@ -0,0 +1,4 @@
+module Ai4cr3
+  # VERSION = `shards -v`
+end
+
