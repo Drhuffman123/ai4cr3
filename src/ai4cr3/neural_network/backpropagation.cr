f766890da5db04f6d8c6aa576fb46f9500be0938
Daniel Huffman <drhuffman12@gmail.com>
2026-01-27 17:56:21 -0500

    cleanup

--- src/ai4cr3/neural_network/backpropagation.cr
+++ src/ai4cr3/neural_network/backpropagation.cr
@@ -1,8 +1,7 @@
 # (re-)Ported from:       https://github.com/SergioFierens/ai4r
 
 # require "../data/parameterizable"
-require "./activation_functions"
-
+# require "./activation_functions"
 # require "./weight_initializations"
 
 module Ai4cr3
